c9a027c3eb7f66df52ae4ddde3a8f0bf388320af
Chofongsua <purinat.san@student.mahidol.ac.th>
2020-08-27 17:53:29 +0700

    Create First Dashboard look

---
+++ airquality-next/components/Drawer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import styled from "styled-components";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import MenuIcon from '@material-ui/icons/Menu';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import InfoIcon from '@material-ui/icons/Info';
+
+
+const DashboardRoot = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  color: #fefefe;
+`;
+
+const DrawerListRoot = styled(List)`
+  min-width: 240px;
+  background: #232323;
+  height: 100%;
+  color: #fefefe;
+`;
+
+export default function PersistentDrawerLeft(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const list = () => (
+    <DrawerListRoot style={{ minWidth: 240 }}>
+        <ListItem button key={"Dashboard"}>
+          <ListItemIcon>
+            <DashboardIcon style={{ color: "#fefefe" }} />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button key={"About"}>
+          <ListItemIcon>
+            <InfoIcon style={{ color: "#fefefe" }} />
+          </ListItemIcon>
+          <ListItemText primary="About us" />
+        </ListItem>
+
+    </DrawerListRoot>
+  );
+
+  return (
+    <DashboardRoot>
+      <IconButton
+        style={{ color: "#fefefe" }}
+        onClick={() => setOpen(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <SwipeableDrawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      >
+        {list()}
+      </SwipeableDrawer>
+    </DashboardRoot>
+  );
+}
