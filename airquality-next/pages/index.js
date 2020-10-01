2646c54f8da7afc930a458a3bb42def16b95198f
PurinatG-01 <fame3js49@gmail.com>
2020-10-01 20:35:48 +0700

    Make index.js to be login

--- airquality-next/pages/index.js
+++ airquality-next/pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import React from "react"
 import PageLayout from "../components/PageLayout";
 import Dashboard from "../components/Dashboard";
-
+import Login from "./login"
 export default function Home() {
   return (
-    <PageLayout>
-      <Dashboard />
-    </PageLayout>
+
+    <Login />
+    // <PageLayout>
+    //   <Dashboard />
+    // </PageLayout>
   );
 }
