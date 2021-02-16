1fb26c257e9dc74f56bf7146a02903a285a66690
PurinatG-01 <fame3js49@gmail.com>
2021-02-17 01:37:28 +0700

    Fixing styled-components v5

--- airquality-next/pages/_document.js
+++ airquality-next/pages/_document.js
@@ -23,7 +23,8 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
+          <title>AIRADAR</title>
+          <link rel="shortcut icon" href="/static/Logo_1.svg" />
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
