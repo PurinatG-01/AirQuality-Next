8cfe88a2ce97ef239fe2a36b68f78e3f56b0c8cc
PurinatG-01 <fame3js49@gmail.com>
2021-02-17 00:42:12 +0700

    Fixing styled-components v2

--- airquality-next/pages/_document.js
+++ airquality-next/pages/_document.js
@@ -1,7 +1,56 @@
 import Document, { Html, Head,  Main, NextScript } from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const sheet = new ServerStyleSheet()
+//     const originalRenderPage = ctx.renderPage
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         })
+
+//       const initialProps = await Document.getInitialProps(ctx)
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       }
+//     } finally {
+//       sheet.seal()
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <title>MUICT Senior 2020 : Air Quality </title>
+//           <link rel="shortcut icon" href="/static/Logo_1.svg" />
+//         </Head>
+//         <body style={{margin:0,}}>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     )
+//   }
+// }
+
+// export default MyDocument
+
+
+// import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -28,20 +77,18 @@ class MyDocument extends Document {
     }
   }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <title>MUICT Senior 2020 : Air Quality </title>
-          <link rel="shortcut icon" href="/static/Logo_1.svg" />
-        </Head>
-        <body style={{margin:0,}}>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+  //   render() {
+  //   return (
+  //     <Html>
+  //       <Head>
+  //         <title>MUICT Senior 2020 : Air Quality </title>
+  //         <link rel="shortcut icon" href="/static/Logo_1.svg" />
+  //       </Head>
+  //       <body style={{margin:0,}}>
+  //         <Main />
+  //         <NextScript />
+  //       </body>
+  //     </Html>
+  //   )
+  // }
 }
-
-export default MyDocument
